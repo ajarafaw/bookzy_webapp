9939786043eb43a2a9f4bbe086171a1e58b2e5af
Wafa Raja <wafa.raja@ngc.com>
2021-02-12 17:37:37 -0500

    first commit

---
+++ src/main/java/bookzy_webapp/BookzyWebappApplication.java
@@ -0,0 +1,13 @@
+package bookzy_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bookzy_webapp/BookzyWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package bookzy_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
