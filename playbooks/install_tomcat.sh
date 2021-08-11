6dad728cec151d021ebc0be555cb837e08c17dfe
Cloud User <you@example.com>
2021-08-11 07:33:40 -0400

    update

---
+++ playbooks/install_tomcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Install tomcat
+yum install java -y
+groupadd tomcat
+useradd -s /bin/false -g tomcat -d /home/tomcat tomcat
+cd /tmp
+curl -O https://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-8/v8.5.69/bin/apache-tomcat-8.5.69.tar.gz
+mkdir /opt/tomcat
+tar -zxvf apache-tomcat-8.5.69.tar.gz -C /opt/tomcat --strip-components=1
+chgrp -R tomcat /opt/tomcat;chmod -R g+r conf;chmod g+x conf;chown -R tomcat /opt/tomcat;chown -R tomcat webapps/ work/ temp/ logs/
+yum update -y
---
+++ tomcat/tomcat_conf_files_updates.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# tomcat configuration files to copy from git repo. First copy the repo
+# git@github.com:ajarafaw/bookzy_webapp.git
+cd ~/bookzy_webapp/tomcat
+yes | cp -p tomcat.service /etc/systemd/system/
+yes | cp -p tomcat-users.xml /opt/tomcat/conf/
+yes | cp -p server.xml /opt/tomcat/conf/
+yes | cp -p context_manager.xml /opt/tomcat/webapps/manager/META-INF/context.xml
+yes | cp -p context_host-manager.xml /opt/tomcat/webapps/host-manager/META-INF/context.xml
