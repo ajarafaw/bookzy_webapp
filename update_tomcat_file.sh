15099891930068efd3a542c14c5d2d311da0c03e
root <you@example.com>
2021-08-11 01:38:48 +0000

    add

--- update_tomcat_file.sh
+++ update_tomcat_file.sh
@@ -1,6 +1,5 @@
-yes | cp -pf tomcat.service /etc/systemd/system
-yes | cp -p tomcat.service /etc/systemd/system
-yes | cp -p tomcat-users.xml /opt/tomcat/conf
+yes | cp -p tomcat.service /etc/systemd/system/
+yes | cp -p tomcat-users.xml /opt/tomcat/conf/
 yes | cp -p server.xml /opt/tomcat/conf/
 yes | cp -p context_manager.xml /opt/tomcat/webapps/manager/META-INF/context.xml
 yes | cp -p context_host-manager.xml /opt/tomcat/webapps/host-manager/META-INF/context.xml
