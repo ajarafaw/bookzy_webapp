47597342dd112042b992375853ae29a9e06d25dd
Cloud User <you@example.com>
2021-08-11 07:34:16 -0400

    update

--- tomcat/update_tomcat_conf_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# tomcat configuration files to copy from git repo. First copy the repo
-# git@github.com:ajarafaw/bookzy_webapp.git
-
-yes | cp -p tomcat.service /etc/systemd/system/
-yes | cp -p tomcat-users.xml /opt/tomcat/conf/
-yes | cp -p server.xml /opt/tomcat/conf/
-yes | cp -p context_manager.xml /opt/tomcat/webapps/manager/META-INF/context.xml
-yes | cp -p context_host-manager.xml /opt/tomcat/webapps/host-manager/META-INF/context.xml
